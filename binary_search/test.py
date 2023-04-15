84ded7d6b68c9a9ffee4c4f76e2f067ebe10c83e
Keith Noguchi <keith@noguchi.us>
2023-04-15 16:52:49 -0700

    selection_sort: A selection sort (#4)

--- binary_search/test.py
+++ binary_search/test.py
@@ -1,9 +1,7 @@
-from unittest import TestCase
-from main import binary_search
+from binary_search.main import binary_search
 
-class Testing(TestCase):
-    def test(self):
-        my_list = [1, 3, 5, 7, 9]
-        assert binary_search(my_list, 5) == 2
-        assert binary_search(my_list, 1) == 0
-        assert binary_search(my_list, 2) is None
+def test():
+    my_list = [1, 3, 5, 7, 9]
+    assert binary_search(my_list, 5) == 2
+    assert binary_search(my_list, 1) == 0
+    assert binary_search(my_list, 2) is None
---
+++ selection_sort/test.py
@@ -0,0 +1,4 @@
+from selection_sort.main import selection_sort
+
+def test():
+    assert selection_sort([3, 4, 5, 2, 1]) == [1, 2, 3, 4, 5]
